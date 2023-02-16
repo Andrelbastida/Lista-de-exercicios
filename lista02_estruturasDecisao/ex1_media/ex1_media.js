ead3d67f4da983ed0e953f69e8982bfe8925d2d4
andrelbastida <andrelbastida@yahoo.com.br>
2023-02-16 18:59:45 -0300

    exerciocio 2 lista 2

--- lista02_estruturasDecisao/ex1_media/ex1_media.js
+++ lista02_estruturasDecisao/ex1_media/ex1_media.js
@@ -5,13 +5,14 @@ btn.addEventListener("click",calcularMedia)
 
 function calcularMedia(){
 
-    var n1 = Number(document.getElementById('n1').value)
-    var n2 = Number(document.getElementById('n2').value)
+    let n1 = Number(document.getElementById('n1').value)
+    let n2 = Number(document.getElementById('n2').value)
 
-var calculo = ( n1 + n2 ) / 2
-if (calculo > 5){
-    document.getElementById("result").innerText= "Aprovado"
+    let media = ( n1 + n2 ) / 2
+
+    if (media >= 5){
+    document.getElementById("result").innerText= "Aprovado" + calculo
 }
-}else{
-    document.getElementById("result").innerText= "Reprovado"
+    }else{
+    document.getElementById("result").innerText= "Reprovado" + calculo
 }
