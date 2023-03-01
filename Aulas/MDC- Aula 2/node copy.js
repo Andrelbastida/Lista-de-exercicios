3f5596cfa10afa0c607a6bfac44760b512f595ab
andrelbastida <andrelbastida@yahoo.com.br>
2023-03-01 20:22:32 -0300

    aula 02

---
+++ Aulas/MDC- Aula 2/node copy.js
@@ -0,0 +1,30 @@
+var btn = document.getElementById("btnCalcular")
+btn.addEventListener("click",calcular)
+
+var result = document.getElementById("result")
+
+function calcular (){
+        let n1 = parseInt(document.getElementById("n1").value)
+        let n2 = parseInt(document.getElementById("n2").value)      
+
+        if ( n1 > n2 ){
+
+            mdc(n1,n2)
+
+        }else{
+            mdc(n2,n1)
+        }
+
+    }
+
+    function mdc(maior , menor){
+        
+
+        for (let ctrl = menor ; ctrl >= 1 ; ctrl -- ){
+            if ( maior % ctrl == 0 && menor % ctrl == 0){
+                result.innerText=ctrl
+                break 
+            }
+
+        }
+    }
--- Aulas/MDC- Aula 2/node.js
+++ Aulas/MDC- Aula 2/node.js
@@ -1,18 +1,29 @@
-var btn = document.getElementById('btnCalcular')
+var btn = document.getElementById("btnCalcular")
 btn.addEventListener("click",mdc)
 
 function mdc (){
-    let n1 = parseInt(document.getElementById("n1").value)
-    let n2 = parseInt(document.getElementById("n2").value)
-    let result = document.getElementById("result")
-    let ctrl
+        let n1 = parseInt(document.getElementById("n1").value)
+        let n2 = parseInt(document.getElementById("n2").value)
+        let result = document.getElementById("result")
+        let ctrl
 
-    if ( n1 > n2 ){
-        // para variavel "ctrl" com valor de n2, enquanto ctrl >= 1, ctrl decrementa de 1 em 1
-        for (let ctrl = n2 ; ctrl >= 1 ; ctrl -- ){
-            if ( n1 % ctrl == 0 && n2 % ctrl == 0)
+        if ( n1 > n2 ){
+            // para variavel "ctrl" com valor de n2, enquanto ctrl >= 1, ctrl decrementa de 1 em 1
+            for (ctrl = n2 ; ctrl >= 1 ; ctrl -- ){
+
+                if ( n1 % ctrl == 0 && n2 % ctrl == 0){
+                    result.innerText=ctrl
+                    break   
+                }
+            }
+        }else{
+            for (let ctrl = n1 ; ctrl >= 1 ; ctrl -- ){
+                if ( n1 % ctrl == 0 && n2 % ctrl == 0){
+                    result.innerText=ctrl
+                    break 
+                }
+
         }
-    }else{
 
     }
 
