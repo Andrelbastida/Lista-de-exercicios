d5c2ac06321d0af347144988ee8c44e4ddc54481
andrelbastida <andrelbastida@yahoo.com.br>
2023-03-01 22:42:29 -0300

    metodos recursivos

--- Aulas/MDC- Aula 2/2-node copy.js
+++ Aulas/MDC- Aula 2/2-node copy.js
@@ -5,8 +5,8 @@ var result = document.getElementById("result")
 
 function calcular (){
         let n1 = parseInt(document.getElementById("n1").value)
-        let n2 = parseInt(document.getElementById("n2").value)      
-
+        let n2 = parseInt(document.getElementById("n2").value)
+     
         if ( n1 > n2 ){
 
             mdc(n1,n2)
